ddd93d771a681d3f5524a26a47f8cae566aef20a
Harshit Padalia <harshit.padalia@letsnurture.com>
2017-03-01 15:44:43 +0530

    new Custom view and textfield added.

--- first/Classes/CustomUIView.swift
+++ first/Classes/CustomUIView.swift
@@ -5,20 +5,41 @@
 //  Created by Lokesh on 27/02/17.
 //
 //
-
+//
 import Foundation
 import UIKit
 @IBDesignable
-
-public class CustomUIView:UIView
+public class customview: UIView
 {
-    var userimagename: UIColor!
-    @IBInspectable var imagename = " " {
-        didSet{
-            
-            
+    
+    // PARAM :- - view radious changer
+    
+    @IBInspectable var radious: Float32 = 0.0
+        {
+        didSet {
+            self.layer.cornerRadius = CGFloat(radious)
         }
-        
+    }
+    
+    // PARAM :-  - View cornerradious change at spefic angle of view
+    
+    @IBInspectable var cornersteter: Float32 = 0.0
+    @IBInspectable var borderradiousseter: Float32 = 0.0
+        {
+        didSet{
+            self.roundCorners(corners: UIRectCorner(rawValue: UInt(cornersteter)), radius: CGFloat(borderradiousseter))
+        }
+    }
+    // PARAM :- Functions
+    
+    func roundCorners(corners:UIRectCorner, radius: CGFloat) {
+        let path = UIBezierPath(roundedRect: self.bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        let mask = CAShapeLayer()
+        mask.path = path.cgPath
+        self.layer.mask = mask
     }
     
 }
+
+
+
